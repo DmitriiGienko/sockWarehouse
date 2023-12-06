abdc8521b0d9d5036368617e4ef92ac0d98b8d2d
DmitriiGienko <110658476+DmitriiGienko@users.noreply.github.com>
2023-12-06 12:54:06 +0300

    initial

---
+++ sockWarehouse/src/main/java/ru/skypro/my/sockWarehouse/SockWarehouseApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.my.sockWarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockWarehouseApplication.class, args);
+	}
+
+}
---
+++ sockWarehouse/src/test/java/ru/skypro/my/sockWarehouse/SockWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.my.sockWarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
