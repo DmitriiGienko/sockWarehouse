ebfaa64551f17734df3fb09da687f64b846f1d47
DmitriiGienko <110658476+DmitriiGienko@users.noreply.github.com>
2023-12-08 11:49:37 +0300

    Подправил репозиторий

---
+++ sockWarehouse/src/test/java/ru/skypro/my/sockWarehouse/controller/SockControllerTest.java
@@ -0,0 +1,36 @@
+package ru.skypro.my.sockWarehouse.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SockControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addSocks() {
+    }
+
+    @Test
+    void removeSocks() {
+    }
+
+    @Test
+    void getCountSocks() {
+    }
+}
